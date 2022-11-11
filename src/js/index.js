0db9dd8db1a09f15866bc21e4c9a71faa2720549
Edwin Harmata <edwin.harmata@gmail.com>
2022-11-11 20:47:57 +0100

    Finish base layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
-console.log('Hello world');
+// GLOBAL VARIABLES
+const MENU_BREAKPOINT = 768;
+const MENU_OPENED_CLASS = 'header__nav--open';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.querySelector('#toggle');
+    const menu = document.querySelector('#menu');
+    toggleButton.addEventListener('click', () => {
+        menu.classList.contains(MENU_OPENED_CLASS) ? menu.classList.remove(MENU_OPENED_CLASS) : menu.classList.add(MENU_OPENED_CLASS);
+    })
+});
+
+window.addEventListener('resize', () => {
+    const windowWidth = window.innerWidth;
+    const menu = document.querySelector('#menu');
+    if(windowWidth > MENU_BREAKPOINT) {
+        menu.classList.contains(MENU_OPENED_CLASS) && menu.classList.remove(MENU_OPENED_CLASS);
+    }
+})
